6a4d9dee9824c507f6f913218c7bf1b833ef89df
Leopoldo <97036205+Leo288dev@users.noreply.github.com>
2023-12-21 08:17:25 -0300

    Delete Aulas Guanabara directory

--- Aulas Guanabara/Aula 8 Bibliotecas e importações.py
+++
@@ -1,7 +0,0 @@
-#Exercício Python 16: Crie um programa que leia um número Real
-# qualquer pelo teclado e mostre na tela a sua porção Inteira.
-
-from math import ceil, trunc
-n = float(input('Digite um valor: '))
-inteiro = trunc(n)
-print (f'O valor digitado foi {n} e sua porção inteira foi: {inteiro}')
